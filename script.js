ca8d1b11ad687a796f4db0d75f6bbe84b577b2c5
tamimhossain-cse <tamim2052002@gmail.com>
2026-02-23 00:23:41 +0600

    Just builded the design as figma design and also added script js file and catch some button by using DOM

---
+++ script.js
@@ -0,0 +1,11 @@
+const allBtn = document.getElementById("all-filter-btn");
+const interviewBtn = document.getElementById("interview-filter-btn");
+const rejectedBtn = document.getElementById("rejected-filter-btn");
+
+
+const container = document.getElementById("allCard");
+const cards = document.querySelectorAll(".card");
+
+const totalElement = document.getElementById("total");
+const interviewElement = document.getElementById("interview");
+const rejectedElement = document.getElementById("rejected");
